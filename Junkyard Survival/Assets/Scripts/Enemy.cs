351013fd1c46f7a459f79b99efb33e2fcaf0c6a6
daaniik <danieklassche@gmail.com>
2025-06-16 12:53:00 +0200

    animatie in main scene

--- Junkyard Survival/Assets/Scripts/Enemy.cs
+++ Junkyard Survival/Assets/Scripts/Enemy.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class Enemy : MonoBehaviour
 {
@@ -8,33 +9,59 @@ public class Enemy : MonoBehaviour
     public int coinValue = 10;
     public int damageToBase = 10;
 
+    private bool isDying = false;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     void Update()
     {
-        float move = moveSpeed * Time.deltaTime;
-        transform.Translate(Vector3.forward * move);
-        distanceTraveled += move;
+        if (!isDying)
+        {
+            float move = moveSpeed * Time.deltaTime;
+            transform.Translate(Vector3.forward * move);
+            distanceTraveled += move;
+        }
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Base"))
+        if (other.CompareTag("Base") && !isDying)
         {
             if (Lose.instance != null)
             {
                 Lose.instance.TakeDamage(damageToBase);
             }
 
-            Destroy(gameObject);
+            StartCoroutine(DieAndDestroy());
         }
     }
 
     public void TakeDamage(float amount)
     {
+        if (isDying) return;
+
         health -= amount;
         if (health <= 0f)
         {
             CoinManager.instance.AddCoins(coinValue);
-            Destroy(gameObject);
+            StartCoroutine(DieAndDestroy());
         }
+    }
+
+    private IEnumerator DieAndDestroy()
+    {
+        isDying = true;
+
+        if (animator != null)
+        {
+            animator.SetTrigger("Dood");
+            yield return new WaitForSeconds(2.5f); // Pas aan op basis van je animatieduur
+        }
+
+        Destroy(gameObject);
     }
 }
